15d122eb5695076e9f9c20d8672a2f65ca643fe5
Enhlosweni Enterprise <inhlosoenhle5@gmail.com>
2025-10-06 16:45:18 +0200

    hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,7 +1,8 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+const hamburger = document.getElementById(".hamburger");
+const navLinks = document.getElementById(".nav-links");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   hamburger.classList.toggle("open");
 });
+
